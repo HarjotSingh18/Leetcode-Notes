45ca8b089b592b57ecf8523bea33fe17f55bb731
HarjotSingh18 <harjotmatharu16@gmail.com>
2026-01-01 20:31:04 -0700

    Add twoSum method to Solution class
    Implement twoSum method to find indices of two numbers that add up to target.

--- problems/0001-two-sum/solution.py
+++ problems/0001-two-sum/solution.py
@@ -1,1 +1,12 @@
-
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
+        for x in range(len(nums)):
+            for i in range(1, len(nums)):
+                if nums[x] + nums[i] == target and x != i:
+                    return [x, i]
