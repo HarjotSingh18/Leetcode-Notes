ec7f951c47048f1e677149205331a823fec64e2e
HarjotSingh18 <harjotmatharu16@gmail.com>
2026-01-01 21:06:20 -0700

    Add solution for longest consecutive sequence problem
    Implement longest consecutive sequence algorithm.

--- problems/128. Longest Consecutive Sequence/solution.py
+++ problems/128. Longest Consecutive Sequence/solution.py
@@ -1,1 +1,24 @@
+class Solution(object):
+    def longestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
 
+        if not nums:
+            return 0
+
+        nums_set = set(nums)
+        longest = 0
+
+        for i in nums_set:
+            if i - 1 not in nums_set:
+                current = i
+                length = 1
+
+                while current + 1 in nums_set:
+                    current += 1
+                    length += 1
+                longest = max(longest, length)
+                
+        return longest   
