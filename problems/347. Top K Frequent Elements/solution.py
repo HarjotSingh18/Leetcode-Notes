43ad1d3e6294a968c75e9e68fce30041095127ed
Harjot Singh <harjotmatharu16@gmail.com>
2026-01-05 23:14:50 -0700

    Implement topKFrequent method to find frequent elements

--- problems/347. Top K Frequent Elements/solution.py
+++ problems/347. Top K Frequent Elements/solution.py
@@ -1,1 +1,20 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        
+        dic = defaultdict(int)
+        answ = []
 
+        for key in nums:
+            dic[key] += 1
+
+        sorted_items = sorted(dic.items(), key=itemgetter(1), reverse=True)
+
+        for key, value in sorted_items[:k]:
+            answ.append(key)
+        
+        return answ
